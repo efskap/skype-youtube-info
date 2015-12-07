8f5d15f7e7ead4a98f97fa3c6b4a76553914ed7f
Dmitry Narkevich <efskap@gmail.com>
2015-12-06 22:55:06 -0800

    oops

--- SkyPy/SkyPy/__init__.py
+++
@@ -1,7 +0,0 @@
-from SkyPy.core import Skype
-from SkyPy.conn import SkypeConnection
-from SkyPy.user import SkypeUser, SkypeContact, SkypeRequest
-from SkyPy.chat import SkypeChat, SkypeSingleChat, SkypeGroupChat, SkypeMsg, SkypeContactMsg, SkypeImageMsg
-from SkyPy.event import SkypeEvent, SkypeTypingEvent, SkypeMessageEvent, SkypeNewMessageEvent, SkypeEditMessageEvent
-from SkyPy.bot import SkypeBot
-from SkyPy.util import SkypeException, SkypeApiException
